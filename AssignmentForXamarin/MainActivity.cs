1b650653269aeb187de5e0ba1b8c36c446bee846
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-14 11:20:09 -0400

    More work with the UI

--- AssignmentForXamarin/MainActivity.cs
+++ AssignmentForXamarin/MainActivity.cs
@@ -12,7 +12,7 @@ namespace AssignmentForXamarin
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
